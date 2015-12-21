941375a9b429e5e6a42239c3977b7c8d359afe6c
亓鑫 <Qixin@qixindeimac.local>
2015-12-21 18:06:38 +0800

    提交文件

---
+++ WeBusiness/WeBusiness/Controllers/Goods/Main/QXGoodsMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  QXGoodsMainViewController.h
+//  WeBusiness
+//
+//  Created by 亓鑫 on 15/12/18.
+//  Copyright © 2015年 亓鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QXGoodsMainViewController : UITableViewController
+
+@end
