77325ba4e8ddc7aa12a0a57b3befae482bae77d0
亓鑫 <Qixin@qixindeimac.local>
2015-12-28 11:54:44 +0800

    移动路径

---
+++ WeBusiness/WeBusiness/Controllers/Orders/Main/QXOrderMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  QXOrderMainViewController.h
+//  WeBusiness
+//
+//  Created by 亓鑫 on 15/12/18.
+//  Copyright © 2015年 亓鑫. All rights reserved.
+//
+
+#import "QXBaseTableViewController.h"
+
+@interface QXOrderMainViewController : QXBaseTableViewController
+
+@end
